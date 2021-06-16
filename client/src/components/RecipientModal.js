57e2876d79210f8167c3db419efb907c8952eb4a
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-06-16 14:55:59 -0400

    implemented front end for selecting recipient list

--- client/src/components/RecipientModal.js
+++ client/src/components/RecipientModal.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { makeStyles } from '@material-ui/core/styles';
+import { CircularProgress } from "@material-ui/core";
+import DoneIcon from '@material-ui/icons/Done';
+import RecipientRow from "./RecipientRow";
 
 function ReciepientModal(props) {
     const [modalStyle, setModalStyle] = useState(getModalStyle)
+    const [recipientList, setReciepientList] = useState([])
+    const [allSelected, setAllSelected] = useState(false)
+    const [noneSelected, setNoneSelected] = useState(true)
+    const [isSaving, setIsSaving] = useState(false)
+    const [doneSaving, setDoneSaving] = useState(false)
 
+    useEffect(() => {
+        async function getRecipients() {
+            const recipients = ["test@test.com", "test2@test.com", "test3@test.com"]
+            setReciepientList(recipients)
+        }
+        getRecipients()
+
+    }, [])
+
+    // styles
     function getModalStyle() {
         return {
             top: `30%`,
@@ -23,9 +41,49 @@ function ReciepientModal(props) {
 
     }));
     const classes = useStyles();
-    return <div style={modalStyle} className={classes.paper}>
-      
-    </div>
+
+    // helper functions 
+    function buildRecipients() {
+        const final = []
+        for (var i = 0; i < recipientList.length; i++) {
+            final.push(
+                <RecipientRow recipient={recipientList[i]} allSelected={allSelected} noneSelected={noneSelected} toggle={toggle} />
+            )
+        }
+        return final
+    }
+    function selectAllRecipients() {
+        setNoneSelected(false)
+        setAllSelected(true)
+
+    }
+    function selectNoRecipients() {
+        setAllSelected(false)
+        setNoneSelected(true)
+    }
+    function toggle() {
+        setAllSelected(false)
+        setNoneSelected(false)
+    }
+    function saveRecipients(){
+        setIsSaving(true)
+    }
+
+    // component render
+    return (
+        <div style={modalStyle} className={classes.paper}>
+            <h1>Select Recipients</h1>
+            <div>
+                <button onClick={selectAllRecipients}> Select All</button>
+                <button onClick={selectNoRecipients}> Deselect All</button>
+            </div>
+        
+            {buildRecipients()}
+          
+            <div className="d-flex mb-3">
+                <button className="btn btn-primary ms-auto" onClick={saveRecipients}>{isSaving ? <CircularProgress color="black" /> : doneSaving ? <DoneIcon /> : "Save Edit"} </button>
+            </div>
+        </div>)
 }
 
 export default ReciepientModal
---
+++ client/src/components/RecipientRow.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react"
+
+
+function RecipientRow(props) {
+
+    const [isSelected, setIsSelected] = useState(false)
+
+    useEffect(() => {
+        if (props.allSelected) {
+            setIsSelected(true)
+        }
+        if (props.noneSelected) {
+            setIsSelected(false)
+        }
+    }, [props.allSelected, props.noneSelected])
+    function toggle(){
+        props.toggle()
+        setIsSelected(!isSelected)
+    }
+    return (
+        <div class="form-check">
+            <label class="form-check-label" for={`${props.recipient}`}>
+                {props.recipient}
+            </label>
+            <input class="form-check-input" type="checkbox" value="" id={`${props.recipient}`} checked={isSelected} onClick={toggle}/>
+        </div>
+    )
+}
+
+export default RecipientRow
