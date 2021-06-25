f8238cf9ffcf4b37d1b9a78fe74e5d924ff513de
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-06-24 21:29:23 -0400

    began work on front page

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -6,25 +6,13 @@ import "./homepage.css"
 function Homepage() {
 
     return (
-        <div>
-            <Navbar active="home"/>
-            <div className="container">
-                <Grow in={true}>
-                    <div className="row" id="TitleBlock">
-                        <div className="col-8">
-                            <div className="row">
-                                <h1>Welcome to the Farm</h1>
-                            </div>
-                            <div className="row">
-                                <p>Description about farm goes here</p>
-                            </div>
-                        </div>
-                        <div className="col-4" style={{ padding: '0px' }}>
-                            <img src="https://blog.ed.gov/files/2019/08/AdobeStock_221344370.jpeg" style={{ width: "100%", padding: "0px" }} />
-                        </div>
-                    </div>
-                </Grow>
+        <div >
+            <Navbar active="home" />
+            <div className="homePageBanner d-flex justify-content-center" style={{backgroundImage: `url("images/interior1.jpg")`}}>
+                <img src="images/logo.png" alt="Farm Logo" id="farmLogo"/>
             </div>
+       
+     
 
         </div>
 
