a8af49134217d5fcfaf23ca637e502a4ae3ab8f7
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-06-26 21:50:08 -0400

    added fade in title on homepage, darkened background image, began work on first section of homepage

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Grow from '@material-ui/core/Grow';
+import Fade from '@material-ui/core/Fade'
 import Navbar from "../../components/Navbar"
 import "./homepage.css"
 
@@ -8,11 +9,26 @@ function Homepage() {
     return (
         <div >
             <Navbar active="home" />
-            <div className="homePageBanner d-flex justify-content-center" style={{backgroundImage: `url("images/landingpage.jpg")`}}>
-                <img src="images/logo.png" alt="Farm Logo" id="farmLogo"/>
+            <div className="homePageBanner" style={{ backgroundImage: `url("images/landingpage.jpg")` }}>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <img src="images/logo.png" alt="Farm Logo" id="farmLogo" />
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-center">
+                    <div style={{ width: "30vw" }}>
+                        <div className="" >
+                            <Fade in={true} timeout={1000}>
+                                <h1 className="text-center" id="landingPageTitle">Welcome to the O'Connor Family Farm</h1>
+                            </Fade>
+                        </div>
+                    </div>
+                </div>
             </div>
-       
-     
+            <div className="ourHistory">
+                <h1 className="text-center">The History </h1>
+            </div>
+
 
         </div>
 
