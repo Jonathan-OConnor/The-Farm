9c39c5a76b2c0c3ae054067d28b250a9a16ac407
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-06-28 20:25:34 -0400

    text now fades in on homepage

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -10,16 +10,20 @@ function Homepage() {
         <div >
             <Navbar active="home" />
             <div className="homePageBanner" style={{ backgroundImage: `url("images/landingpage.jpg")` }}>
-                <div className="row">
-                    <div className="col d-flex justify-content-center">
-                        <img src="images/logo.png" alt="Farm Logo" id="farmLogo" />
-                    </div>
-                </div>
+
                 <div className="row d-flex justify-content-center">
                     <div style={{ width: "30vw" }}>
                         <div className="" >
                             <Fade in={true} timeout={1000}>
-                                <h1 className="text-center" id="landingPageTitle">Welcome to the O'Connor Family Farm</h1>
+                                <div>
+                                    <div className="row">
+                                        <div className="col d-flex justify-content-center">
+                                            <img src="images/logo.png" alt="Farm Logo" id="farmLogo" />
+                                        </div>
+                                    </div>
+                                    <h1 className="text-center" id="landingPageTitle">Welcome to the O'Connor Family Farm</h1>
+                                </div>
+
                             </Fade>
                         </div>
                     </div>
